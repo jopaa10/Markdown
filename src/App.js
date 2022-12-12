020d5c6d7034e63f3c726bab645d047950cf5f7a
Josipa Znaor <josipaznaor@MacBook-Pro-od-Josipa.fritz.box>
2022-12-12 15:15:41 +0100

    creating markdown and adding save to pdf feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useRef, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import { PDFExport, savePDF } from "@progress/kendo-react-pdf";
 
-function App() {
-  return <h2>markdown preview starter</h2>
+export function App() {
+  const [markdown, setMarkdown] = useState("## markdown preview");
+
+  const pdfExportComponent = useRef(null);
+
+  const handleSaveToPdf = (event) => {
+    pdfExportComponent.current.save();
+  };
+
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        />
+        <PDFExport ref={pdfExportComponent} paperSize="A4">
+          <article className="result">
+            <ReactMarkdown>{markdown}</ReactMarkdown>
+          </article>
+        </PDFExport>
+        <div className="saveContainer">
+          <button className="save" onClick={handleSaveToPdf}>
+            Save to PDF
+          </button>
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
